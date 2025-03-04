4cd75d141cc2cba622dab6c55a925476996cbc29
SAFRAN MHD <mohammedsafran6ix@gmail.com>
2025-03-04 15:16:42 +0530

    Add files via upload

---
+++ BookReg.java
@@ -0,0 +1,20 @@
+public class BookReg{
+
+    private String title;
+    private String author;
+    private String ISBN;
+    private double price;
+
+    public BookReg(String title, String author){
+        this.title = title;
+        this.author = author;
+        this.ISBN = "Not Available!";
+        this.price = 0.0;
+    }
+
+
+   public static void main(String[] args)
+   {
+       System.out.println("Hello World");
+   }
+}
