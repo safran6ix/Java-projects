b73544efd085108257a55266fd4c0cc6b4cd309b
safran6ix <mohammedsafran6ix@gmail.com>
2025-03-04 15:47:40 +0530

    Modified

--- BookReg.java
+++ BookReg.java
@@ -1,10 +1,12 @@
 public class BookReg{
 
+    //Atributes
     private String title;
     private String author;
     private String ISBN;
     private double price;
 
+    //Constructor 01
     public BookReg(String title, String author){
         this.title = title;
         this.author = author;
@@ -12,6 +14,7 @@ public class BookReg{
         this.price = 0.0;
     }
     
+    //Constructor 02
     public BookReg(String title, String author, String ISBN){
         this.title = title;
         this.author = author;
@@ -19,8 +22,34 @@ public class BookReg{
         this.price = 0.0;
     }
 
-   public static void main(String[] args)
+    //Constructor 03
+    public BookReg(String title, String author, String ISBN, double price){
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.price = price;
+    }
+
+    //Method 01
+    public String getBookDetails(){
+        return "Title: " + title + "Author: " + author + "ISBN: " + ISBN + "Price : $ " + price;
+    }
+    
+    //Method 02
+    public String getBookDetails(String ISBN){
+        if(this.ISBN.equals(ISBN)){
+            return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Price: $" + price; 
+        }
+        else{
+            return "No book found with ISBN: " + ISBN;
+        }
+    }
+
+    public static void main(String[] args)
    {
-       System.out.println("Hello World");
+       
+    
+
+    
    }
 }
