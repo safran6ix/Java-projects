f940aba26f348fd5a48d72d8bc267ee865205b79
SAFRAN MHD <mohammedsafran6ix@gmail.com>
2025-03-14 23:30:17 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,20 @@
+
+ import java.util.*;
+public class Hello{
+    public static void main(String[] args){
+
+        Stack <Integer> st = new Stack<>();
+
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+
+        int r = st.pop();
+        System.out.println(r);
+        System.out.println(st);
+
+    }
+}
