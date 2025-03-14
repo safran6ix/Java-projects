8e7025a7e4600ded062ab2315e548e73f2010f16
safran6ix <mohammedsafran6ix@gmail.com>
2025-03-14 23:41:02 +0530

    Modified

--- StackConcept.java
+++ StackConcept.java
@@ -5,6 +5,7 @@ public class StackConcept{
 
         Stack <Integer> st = new Stack<>();
 
+        //PUSH
         st.push(1);
         st.push(2);
         st.push(3);
@@ -12,13 +13,20 @@ public class StackConcept{
         st.push(5);
         System.out.println(st);
 
+        //POP
         int r = st.pop();
         System.out.println(r);
         System.out.println(st);
 
+        //PEEK
         int p = st.peek();
         System.out.println(p);
         System.out.println(st);
+
+        //EMPTY
+        boolean e = st.empty();
+        System.out.println(e);
+
 
     }
 }
