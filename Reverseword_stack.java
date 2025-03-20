43e7776ea2893a17b3de7bca9b3713cbcf67ea45
safran6ix <mohammedsafran6ix@gmail.com>
2025-03-20 20:07:49 +0530

    Modified

--- Reverseword_stack.java
+++ Reverseword_stack.java
@@ -1,12 +1,17 @@
-
+//Word Reversed character from user input
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Reverseword_stack{
     public static void main(String[] args)
     {
 
-        String str = "Hello";
+//        String str = "Hello";
         Stack <Character> stack = new Stack<>();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a word : ");
+        String str = scan.nextLine();
 
         for (char c : str.toCharArray()){
             stack.push(c);
