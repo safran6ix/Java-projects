72cadc685f94eae5d557bc76071563e915139a90
safran6ix <mohammedsafran6ix@gmail.com>
2025-03-20 20:48:00 +0530

    Modified

--- Defstack.java
+++ Defstack.java
@@ -1,11 +1,28 @@
-class minstack{
 
+import java.util.Stack;
+
+class MinStack{
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack(){
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x){
+        stack.push(x);
+        if(minStack.isEmpty() || x <= minStack.peek()){
+           minStack.push(x);
+        }
+    }
 }
 
 public class Defstack{
     public static void main(String[] args)
     {
 
+        MinStack minStack = new MinStack();
 
 
     }
