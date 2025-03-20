eab149c4e2e6dc470c9ff618a2763f1fe7eeb609
safran6ix <mohammedsafran6ix@gmail.com>
2025-03-20 20:22:38 +0530

    Modified

---
+++ Defstack.java
@@ -0,0 +1,12 @@
+class minstack{
+
+}
+
+public class Defstack{
+    public static void main(String[] args)
+    {
+
+
+
+    }
+}
--- Minstack.java
+++
@@ -1,8 +0,0 @@
-public class Minstack{
-    public static void main(String[] args)
-    {
-
-
-
-    }
-}
