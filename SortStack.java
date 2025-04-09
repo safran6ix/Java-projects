8f587a6d8bb05b3dfcf96a51e52dc4339c26c2de
SAFRAN MHD <mohammedsafran6ix@gmail.com>
2025-04-09 23:13:36 +0530

    Add files via upload

---
+++ SortStack.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+class SortStack {
+    public static void sort(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+
+        int temp = stack.pop();
+        sort(stack);
+        insertSorted(stack, temp);
+    }
+
+    private static void insertSorted(Stack<Integer> stack, int value) {
+        if (stack.isEmpty() || value < stack.peek()) {
+            stack.push(value);
+            return;
+        }
+
+        int temp = stack.pop();
+        insertSorted(stack, value);
+        stack.push(temp);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(30);
+        stack.push(10);
+        stack.push(50);
+        stack.push(20);
+        stack.push(70);
+        stack.push(80);
+        stack.push(120);
+        stack.push(140);
+
+        sort(stack);
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+        // Output will be sorted: 10 20 30 50
+    }
+}
