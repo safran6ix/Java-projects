3dbb624b42bd237b1b9c5c8f57b8973c68483d88
safran6ix <mohammedsafran6ix@gmail.com>
2025-05-25 15:14:34 +0530

    Add

--- JavaGUI.java
+++ JavaGUI.java
@@ -1,9 +1,15 @@
-import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.*; //GUI components like buttons, frames, etc.
+import java.awt.*; //Layouts and colors.
+import java.awt.event.*; //Event handling like clicks, keypress, etc.
 
 public class JavaGUI{
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+
+        JFrame frame = new JFrame("GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+
     }
 }
