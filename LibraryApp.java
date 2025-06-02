1849f453b6b1f936a409f390d5a66438fcf71d6d
safran6ix <mohammedsafran6ix@gmail.com>
2025-06-03 00:43:28 +0530

    Adding Library class

--- LibraryApp.java
+++ LibraryApp.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
+class LibraryBook{
+    private String bookId;
+    private String title;
+    private String author;
+    private int copiesAvailable;
+}
+
 public class LibraryApp{
-    public static void main(String args[])
+    public static void main(String[] args)
     {
 
        System.out.println("Hello World");
