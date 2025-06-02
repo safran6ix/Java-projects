ef4644e234f68391cfcd13a3209f1e7778636302
safran6ix <mohammedsafran6ix@gmail.com>
2025-06-03 01:13:31 +0530

    Complete

--- LibraryApp.java
+++ LibraryApp.java
@@ -52,6 +52,33 @@ public class LibraryApp{
        int copiesAvailable1 = scan.nextInt();
        scan.nextLine();
 
+       System.out.println("\nEnter Deatials for Book 2:");
+       System.out.print("Book ID2: ");
+       String bookId2 = scan.nextLine();
+
+       System.out.print("Title2: ");
+       String title2 = scan.nextLine();
+
+       System.out.print("Author2: ");
+       String author2 = scan.nextLine();
+
+       System.out.print("Copies Available2: ");
+       int copiesAvailable2 = scan.nextInt();
+
+       LibraryBook book1 = new LibraryBook(bookId1, title1, author1, copiesAvailable1);
+       LibraryBook book2 = new LibraryBook(bookId2, title2, author2, copiesAvailable2);
+
+       System.out.println("\nEnter number of copies to borrow for Book1: ");
+       int borrow1 = scan.nextInt();
+       book1.lendBook(borrow1);
+
+       System.out.print("Enter number of copies to borrow for Book2: ");
+       int borrow2 = scan.nextInt();
+       book2.lendBook(borrow2);
+
+       System.out.println("\n--- Book Details After Lending ---");
+       book1.printBookDetails();
+       book2.printBookDetails();
 
     }
 }
