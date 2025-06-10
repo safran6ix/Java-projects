655f166a4a62d2c4689642b303ae5fac55e96eb6
safran6ix <mohammedsafran6ix@gmail.com>
2025-06-10 19:00:09 +0530

    Modified

--- Transport_App.java
+++ Transport_App.java
@@ -1,6 +1,28 @@
+class Vehicle{
+    protected String brand;
+    protected double speed;
+    protected Engine engine;
+
+    public Vehicle(String brand, double speed, String engineType){
+        this.brand = brand;
+        this.speed = speed;
+        this.engine = new Engine(engineType);
+    }
+
+    public void start(){
+        System.out.println("The vehicle is starting...");
+    }
+
+    public void displayDetails(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Speed: " + speed + " km/h");
+        engine.displayEngineInfo();
+    }
+}
+
 public class Transport_App{
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+
     }
 }
