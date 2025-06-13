18030f0edbfc6d2c5e6e1befc219bb6feb221be4
safran6ix <mohammedsafran6ix@gmail.com>
2025-06-13 19:02:35 +0530

    Username Error

--- TryCatchException.java
+++ TryCatchException.java
@@ -16,5 +16,20 @@ public class TryCatchException{
     public static void main(String[] args)
     {
 
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            //Prompt user for username
+            System.out.print("Enter username: ");
+            String username = scan.nextLine();
+
+            //Validate username
+            if(username.length() < 6){
+                throw new InvalidUserName();
+            }
+        }
+        catch (InvalidUserName e){
+            System.out.println("Username Error: " + e.getMessage());
+        }
     }
 }
