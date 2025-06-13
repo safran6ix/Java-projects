8585fdbc1b3e1084ded5e190cb9fbc0ed5da147a
safran6ix <mohammedsafran6ix@gmail.com>
2025-06-13 18:58:19 +0530

    Adding classes

--- TryCatchException.java
+++ TryCatchException.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
+class InvalidUserName extends Exception{
+    public InvalidUserName(){
+        super("The username must be a at least 6 characters long");
+    }
+}
+
+class InvalidPasswordLength extends Exception{
+    public InvalidPasswordLength(){
+        super("The password must be at least 8 characters long");
+    }
+}
+
 public class TryCatchException{
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+
     }
 }
