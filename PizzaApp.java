f0614519c220ea6a096baab97b3af1f34ff281af
safran6ix <mohammedsafran6ix@gmail.com>
2025-06-21 12:36:23 +0530

    Initial attribute

--- PizzaApp.java
+++ PizzaApp.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
+class PizzaOrder{
+    private String orderID;
+    private String customerName;
+    private String size;
+    private int toppingCount;
+    private boolean isDelivery;
+
+    public PizzaOrder(){
+        this.orderID = "N/A";
+        this.customerName = "Unknown";
+        this.size = "Small";
+        this.toppingCount = 0;
+        this.isDelivery = false;
+    }
+}
+
 public class PizzaApp{
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+
+
+
     }
 }
