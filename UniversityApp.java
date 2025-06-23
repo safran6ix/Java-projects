c88b16101a701e077b8d7c2c9ed2132f93b1c2e9
safran6ix <mohammedsafran6ix@gmail.com>
2025-06-23 16:33:33 +0530

    Add office details

--- UniversityApp.java
+++ UniversityApp.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+
+class Office{
+    private String officeNumber;
+    private int capacity;
+
+    public Office(String officeNumber, int capacity){
+        this.officeNumber = officeNumber;
+        this.capacity = capacity;
+    }
+
+    public void displayOfficeDetails(){
+
+    }
+}
+
 public class UniversityApp{
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+
+
+
     }
 }
