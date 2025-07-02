f942df9b95c6c16a1f605e350a3fe173a52d83c6
safran6ix <mohammedsafran6ix@gmail.com>
2025-07-02 21:31:39 +0530

    Adding User Account class

--- ATM.java
+++ ATM.java
@@ -1,8 +1,28 @@
-public class Hello{
-    public static void main (String[] args)
+import java.io.*;
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+class UserAccount{
+    private String accountNumber;
+    private String pin;
+    private double balance;
+
+    public UserAccount(String accountNumber, String pin, double balance){
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.balance = balance;
+    }
+
+
+}
+
+public class ATM{
+    public static void main(String[] args)
     {
-           
-        System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in);
+
 
     }
 }
