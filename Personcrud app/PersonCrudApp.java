cbc0e81424bae265e446dc3d7cbea16513945391
safran6ix <mohammedsafran6ix@gmail.com>
2025-10-13 19:20:50 +0530

    Add do &  Switch

--- Personcrud app/PersonCrudApp.java
+++ Personcrud app/PersonCrudApp.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class PersonCrudApp {
 
+    private static Map<Integer, Person> personMap = new HashMap<>();
     private static int currentId = 1; //ID Generataor
     private static Scanner Scanner = new Scanner(System.in);
 
@@ -15,9 +16,19 @@ public class PersonCrudApp {
             System.out.println("\nPerson Management System");
             System.out.println("1. Create Person");
             System.out.println("3. View Person");
+            
 
+            switch (choice) {
+                case 1:
+                    //CREATE    
 
-       }
+                    break;
+            
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+       } while();
 
     }    
 }
@@ -27,5 +38,5 @@ class Person{
     private int id;
     private String name;
     private int age;
-    
+
 }
