3fc75513ea0c60565ce01851fe9d13f26ec38423
safran6ix <mohammedsafran6ix@gmail.com>
2025-10-13 19:16:41 +0530

    Adding Class

---
+++ Personcrud app/PersonCrudApp.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PersonCrudApp {
+
+    private static int currentId = 1; //ID Generataor
+    private static Scanner Scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+
+        int choice;
+        do{
+          
+            System.out.println("\nPerson Management System");
+            System.out.println("1. Create Person");
+            System.out.println("3. View Person");
+
+
+       }
+
+    }    
+}
+
+
+class Person{
+    private int id;
+    private String name;
+    private int age;
+    
+}
